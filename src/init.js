c4173238861e4cc6dc7e90a3a0fad268441f4f97
gukan <gukan@MacBook-Pro.local>
2020-11-01 21:42:40 +0100

    add infrastructure, initialize the game

---
+++ src/init.js
@@ -0,0 +1,22 @@
+import gameState from "./gameState";
+
+const TICK_RATE = 3000;
+
+const init = async () => {
+  console.log("starting game");
+
+  let nextTimeToTick = Date.now();
+
+  const nextAnimationFrame = () => {
+    const now = Date.now();
+    if (nextTimeToTick <= now) {
+      gameState.tick();
+      nextTimeToTick = now + TICK_RATE;
+    }
+    requestAnimationFrame(nextAnimationFrame);
+  };
+
+  nextAnimationFrame();
+};
+
+init();
